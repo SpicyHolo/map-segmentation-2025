04a31ff230f5574d03ff8bb74249e9dac21521b6
Konrad Makowski <41269364+SpicyHolo@users.noreply.github.com>
2025-01-22 20:11:20 +0100

    add prediction visualisation feature

--- src/models/segmenter_visualizate_utils.py
+++ src/models/segmenter_visualizate_utils.py
@@ -1,4 +1,4 @@
-from neptune.new.types import File
+from neptune.types import File
 import torch
 import random
 from matplotlib.colors import hsv_to_rgb
@@ -20,7 +20,6 @@ def visualize_segmentation_predition(logger, x_s, y_s, y_pred_s, name='test_rand
     palette = get_palette(x_s.shape[1])
 
     for x, y, y_pred in zip(x_s, y_s, y_pred_s):
-
         x = np.uint8(x.permute(1, 2, 0).cpu().numpy()*255)
         y_pred = y_pred.cpu().numpy()
         y_pred = np.argmax(y_pred, axis=0).astype(np.uint8)
@@ -32,9 +31,11 @@ def visualize_segmentation_predition(logger, x_s, y_s, y_pred_s, name='test_rand
 
         frame = cv2.addWeighted(x, 0.3, result_img, 0.7, 0)
         
-        # logger.log_image(
-        #             name,
-        #             File.as_image(np.float32(frame)*1./255.)
-        #         )
+        # Access the underlying Neptune run object
+        neptune_run = logger.experiment
+        neptune_run["images"].log(
+            File.as_image(np.float32(frame)*1./255.),
+            name=name
+        )
 
         break
